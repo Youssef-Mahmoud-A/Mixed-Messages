4cfe839e40402ab741370fdb760608ea1d53acc0
Joe <youssef.mahmoud3002@gmail.com>
2024-09-04 01:57:18 +0300

    First working version

--- Mixed-Messages.js
+++ Mixed-Messages.js
@@ -2,3 +2,25 @@
 function generateRandomNum(num) {
     return Math.floor(Math.random() * num);
 };
+
+//A buffer to hold the final result
+var message = [];
+
+const firstPart = ['You', 'They', 'He', 'She'];
+const secondPart = ['will', 'did', 'will never'];
+const thirdPart = ['love', 'hate', 'die'];
+
+let optionFirst = generateRandomNum(firstPart.length);
+let optionSecond = generateRandomNum(secondPart.length);
+let optionThird = generateRandomNum(thirdPart.length);
+
+console.log(optionFirst);
+console.log(optionSecond);
+console.log(optionThird);
+
+message.push(firstPart[optionFirst]);
+message.push(secondPart[optionSecond]);
+message.push(thirdPart[optionThird]);
+
+formattedMessage = message.join(' ');
+console.log(formattedMessage);
